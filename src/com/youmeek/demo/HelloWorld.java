968325b57e8392a5b3b5750ceacb98fe0585b263
ruiwen <ruiwen1994@hotmail.com>
2017-06-28 16:06:16 +0800

    First Time Commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.youmeek.demo;
+
+/**
+ * Nothing to say.
+ * Created by ruiwen on 2017/6/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
